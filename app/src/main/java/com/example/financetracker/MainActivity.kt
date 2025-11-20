60af7d14aec38adab16101f42e033006ad12cf77
KyleFlynn1 <s00251601@atu.ie>
2025-11-20 11:02:58 +0000

    Basic Setup

--- app/src/main/java/com/example/financetracker/MainActivity.kt
+++ app/src/main/java/com/example/financetracker/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.financetracker
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.financetracker.ui.theme.FinanceTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             FinanceTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SignupScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +39,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun SignupScreen(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+            .padding(24.dp)
+            .background(Color.White)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Sign Up",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.ExtraBold,
+            color = Color.DarkGray,
+            modifier = modifier
+                .align(Alignment.CenterHorizontally)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true )
 @Composable
-fun GreetingPreview() {
+fun SignUpPreview() {
     FinanceTrackerTheme {
-        Greeting("Android")
+        SignupScreen()
     }
 }
