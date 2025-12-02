a32632d2af76a018793ce536895453484cf93dd0
KyleFlynn1 <s00251601@atu.ie>
2025-12-02 18:47:40 +0000

    Added Jetpack Compose Nav

---
+++ app/src/main/java/com/example/financetracker/FinanceScreen.kt
@@ -0,0 +1,60 @@
+package com.example.financetracker
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+// Top app bar composable
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FinanceAppBar(
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit
+) {
+    TopAppBar(
+        title = { Text("Finance Tracker") },
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(Icons.Filled.ArrowBack, contentDescription = null)
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun FinanceApp(
+    navController: NavHostController = rememberNavController()
+) {
+    Scaffold(
+        topBar = {
+            FinanceAppBar(
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() }
+            )
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "signup",
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable("signup") {
+                SignupScreen()
+            }
+            composable("login") {
+                LoginScreen()
+            }
+            composable("home") {
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/financetracker/MainActivity.kt
+++ app/src/main/java/com/example/financetracker/MainActivity.kt
@@ -1,156 +1,18 @@
 package com.example.financetracker
 
-import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.Button
-import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.financetracker.ui.theme.FinanceTrackerTheme
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FinanceTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SignupScreen(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                FinanceApp()
             }
         }
-    }
-}
-
-@Composable
-fun SignupScreen(modifier: Modifier = Modifier) {
-
-    var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-    var confirmPassword by remember { mutableStateOf("") }
-
-    Column(
-        modifier = modifier
-            .padding(24.dp)
-            .background(Color.White)
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Finance Tracker",
-            fontSize = 32.sp,
-            fontWeight = FontWeight.ExtraBold,
-            color = Color.DarkGray,
-            modifier = modifier
-                .align(Alignment.CenterHorizontally)
-        )
-
-        Spacer(Modifier.height(90.dp))
-
-        Text(
-            text = "Sign Up",
-            fontSize = 28.sp,
-            fontWeight = FontWeight.ExtraBold,
-            color = Color.DarkGray,
-            modifier = modifier
-                .align(Alignment.CenterHorizontally)
-        )
-
-        Spacer(Modifier.height(20.dp))
-
-        OutlinedTextField(
-            value = email,
-            onValueChange = {email = it},
-            label = { Text("Email")},
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(Modifier.height(20.dp))
-
-        OutlinedTextField(
-            value = password,
-            onValueChange = {password = it},
-            label = { Text("Password")},
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(Modifier.height(20.dp))
-
-        OutlinedTextField(
-            value = confirmPassword,
-            onValueChange = {confirmPassword = it},
-            label = { Text("Confirm Password")},
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(Modifier.height(35.dp))
-
-        Button(
-            onClick = { onRegister() },
-            modifier = Modifier
-                .width(160.dp)
-                .height(52.dp),
-            shape = RoundedCornerShape(30.dp)
-        ) {
-            Icon(Icons.Default.Star, contentDescription = null)
-            Spacer(Modifier.width(5.dp))
-            Text("Register")
-        }
-
-        Spacer(Modifier.height(20.dp))
-
-        Button(
-            onClick = { onLoginSwitch() },
-            modifier = Modifier
-                .width(250.dp)
-                .height(52.dp),
-            shape = RoundedCornerShape(30.dp)
-        ) { Text("Already Registered? Login") }
-
-    }
-}
-
-fun onRegister() {}
-
-fun onLoginSwitch() {}
-
-@Preview(showBackground = true, showSystemUi = true )
-@Composable
-fun SignUpPreview() {
-    FinanceTrackerTheme {
-        SignupScreen()
     }
 }
