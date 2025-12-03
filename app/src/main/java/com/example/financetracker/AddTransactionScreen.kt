f467d21c7acabb4fbf2c08becd29bab97ae1f476
KyleFlynn1 <s00251601@atu.ie>
2025-12-03 15:18:11 +0000

    Transaction Form Screen

---
+++ app/src/main/java/com/example/financetracker/AddTransactionScreen.kt
@@ -0,0 +1,161 @@
+package com.example.financetracker
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.financetracker.ui.theme.FinanceTrackerTheme
+
+@Composable
+fun TransactionScreen(modifier: Modifier = Modifier) {
+
+    var description by remember { mutableStateOf("") }
+    var cost by remember { mutableStateOf("") }
+    var notes by remember { mutableStateOf("") }
+    var type by remember { mutableStateOf("Deposit") }
+
+    // Transaction type button values
+    val options = listOf("Deposit", "Withdrawal")
+
+    Column(
+        modifier = modifier
+            .padding(24.dp)
+            .background(Color.White)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Finance Tracker",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.ExtraBold,
+            color = Color.DarkGray,
+            modifier = modifier
+                .align(Alignment.CenterHorizontally)
+        )
+        HorizontalDivider()
+        Spacer(Modifier.height(90.dp))
+
+        Text(
+            text = "Add Transaction",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.ExtraBold,
+            color = Color.DarkGray,
+            modifier = modifier
+                .align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = description,
+            onValueChange = {description = it},
+            label = { Text("Description")},
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = cost,
+            onValueChange = {cost = it},
+            label = { Text("Cost")},
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = notes,
+            onValueChange = {notes = it},
+            label = { Text("Notes")},
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(Modifier.height(20.dp))
+
+        Column(modifier = Modifier.padding(16.dp)) {
+            SingleChoiceSegmentedButtonRow(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                options.forEachIndexed { index, label ->
+                    SegmentedButton(
+                        selected = type == label,
+                        onClick = { type = label },
+                        shape = SegmentedButtonDefaults.itemShape(
+                            index = index,
+                            count = options.size
+                        ),
+                        icon = {
+                            if (type == label) {
+                                Icon(
+                                    Icons.Default.Check,
+                                    contentDescription = null,
+                                    modifier = Modifier.size(18.dp)
+                                )
+                            }
+                        }
+                    ) {
+                        Text(label)
+                    }
+                }
+            }
+        }
+
+        Spacer(Modifier.height(20.dp))
+
+        Button(
+            onClick = { onLogin() },
+            modifier = Modifier
+                .width(250.dp)
+                .height(52.dp),
+            shape = RoundedCornerShape(30.dp)
+        ) {
+            Icon(Icons.Default.Star, contentDescription = null)
+            Spacer(Modifier.width(5.dp))
+            Text("Submit Transaction")
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true )
+@Composable
+fun TransactionPreview() {
+    FinanceTrackerTheme {
+        TransactionScreen()
+    }
+}
